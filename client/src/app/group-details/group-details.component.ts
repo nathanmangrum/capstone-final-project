abfab4dadb9ff1115e8dc202bcad7d1bc16151d8
Nathan Mangrum <nathan.mangrum@jbhunt.com>
2022-09-26 17:45:21 -0500

    additions

--- client/src/app/group-details/group-details.component.ts
+++ client/src/app/group-details/group-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Member } from '../models/member';
 import { TastingGroup } from '../models/tasting-group';
+import { MemberService } from '../services/member.service';
+import { TastingGroupService } from '../services/tasting-group.service';
 
 @Component({
   selector: 'app-group-details',
@@ -9,11 +12,22 @@ import { TastingGroup } from '../models/tasting-group';
 export class GroupDetailsComponent implements OnInit {
 
   @Input() groupDetails!: TastingGroup;
+  member!: Member;
 
-  constructor() { }
+  constructor(private readonly groupService: TastingGroupService) { }
 
   ngOnInit() {
     console.log(this.groupDetails);
   }
+  // TODO add modal in template and finish functionality
+  // editMember(member: Member) {
+  //   this.member = {...member};
+  //   this.memberEditModal = true;
+  // }
+  //TODO fix this 
+  // deleteMember(member: Member) {
+  //   const mem = this.groupDetails.Members.find(id => );
+  //   this.groupService.deleteMemberFromTastingGroup();
+  // }
 
 }
